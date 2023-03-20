f8ffe94d318256cd572c07f5ac64b80c7984573e
lzjun <lzjun567@qq.com>
2023-03-20 23:39:15 +0800

    添加测试案例

--- application/blueprint/wechat/views.py
+++ application/blueprint/wechat/views.py
@@ -1,14 +1,14 @@
 import threading
 import time
 
-from flask import current_app, request
+from flask import current_app, request, render_template
 from wechatpy.events import SubscribeScanEvent, ScanEvent
 from wechatpy.exceptions import InvalidSignatureException
 from wechatpy.messages import TextMessage
 from wechatpy.replies import TextReply
 from wechatpy.utils import check_signature
 
-from application.extensions import siwa, cache
+from application.extensions import siwa, cache, openai_api
 from . import wechat_bp
 
 from ...dto import ResponseSuccessDto
@@ -62,3 +62,18 @@ def handler_wx_msg():
     else:
         result = TextReply(content="欢迎使用志军的私人AI助理", message=msg).render()
     return result
+
+
+@wechat_bp.route("/company", methods=["GET", "POST"])
+def handler_company():
+    """
+    根据公司信息生成一句话简介
+    """
+    answer = ""
+    if request.method == 'POST':
+        intro = request.form.get("intro")
+        question = f"'''{intro}'''\n" \
+                   f"基于上面几段话生成一句话简介，格式：xxx商， 必须以 xxx提供商/服务商/销售商/品牌商/运营商/开发商/生产商/研发商/制造商/供应商/平台商 结尾，" \
+                   f"不得超过20个字符，不要标点符号"
+        answer = openai_api.answer(question).strip()
+    return render_template("company.html", answer=answer)
